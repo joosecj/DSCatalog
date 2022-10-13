29ee2b7714ef1d6704f7df0e2171b57066e1bb9e
Jose Carlos <josecarloscjj@gmail.com>
2022-10-12 22:39:47 -0300

    Refactor pageable

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -4,6 +4,8 @@ import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +18,8 @@ public class CategoryResource {
     @Autowired
     private CategoryService service;
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll() {
-        List<CategoryDTO> categoryList = service.findAll();
+    public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable) {
+        Page<CategoryDTO> categoryList = service.findAll(pageable);
         return ResponseEntity.ok(categoryList);
     }
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,9 +18,9 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
     @Transactional(readOnly = true)
-    public List<CategoryDTO> findAll() {
-        List<Category> categoryList = repository.findAll();
-        return categoryList.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+    public Page<CategoryDTO> findAll(Pageable pageable) {
+        Page<Category> categoryList = repository.findAll(pageable);
+        return categoryList.map(x -> new CategoryDTO(x));
     }
 
 }
